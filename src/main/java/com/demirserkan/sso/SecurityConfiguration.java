3a40824052a5908b52fb0d6a235313f752caeef2
Serkan Demir <serkandemirgithub@gmail.com>
2020-11-18 23:31:29 +0300

    custom error

--- src/main/java/com/demirserkan/sso/SecurityConfiguration.java
+++ src/main/java/com/demirserkan/sso/SecurityConfiguration.java
@@ -5,15 +5,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//        http
+//                .authorizeRequests(a -> a
+//                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .exceptionHandling(e -> e
+//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//                )
+//                .oauth2Login(o -> o
+//                    .failureHandler(request, response, exception) -> {
+//                        request.getSession().setAttribute("error.message", exception.getMessage());
+//                        handler.onAuthenticationFailure(request, response, exception);
+//                    })
+//                .and()
+//                .logout(l -> l
+//                        .logoutSuccessUrl("/").permitAll()
+//                )
+//                .csrf(c -> c
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                );
+//        // @formatter:on
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
         // @formatter:off
-        http
+        http.antMatcher("/**")
                 .authorizeRequests(a -> a
                         .antMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
@@ -21,13 +50,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login()
-                .and()
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
                 .logout(l -> l
                         .logoutSuccessUrl("/").permitAll()
                 )
-                .csrf(c -> c
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .oauth2Login(o -> o
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                            handler.onAuthenticationFailure(request, response, exception);
+                        })
                 );
         // @formatter:on
     }
--- src/main/java/com/demirserkan/sso/SsoWithGithubApplication.java
+++ src/main/java/com/demirserkan/sso/SsoWithGithubApplication.java
@@ -3,12 +3,28 @@ package com.demirserkan.sso;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class SsoWithGithubApplication {
 
 	private final SecurityConfiguration securityConfiguration;
+
+	private final Oath2UserService oath2UserService;
+
+
+	@Bean
+	public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+				new ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+		return WebClient.builder()
+				.filter(oauth2).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsoWithGithubApplication.class, args);
